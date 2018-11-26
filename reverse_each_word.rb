230fd23a093e4ad26c38b68b57f34d5627ba6904
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-11-26 05:39:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
-def reverse_each_word(string)
-  original_string = string.split(",")
-  return array = []
-  string.each do |word|
-    puts word.reverse 
-  end
-end
+
 
 def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
